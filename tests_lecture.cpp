a031b9b61e5c403913443a548e28b2de80298b16
BarbaraPerri <perri@ensta.fr>
2015-02-15 22:08:04 +0100

    Update tests_lecture.cpp

--- tests_lecture.cpp
+++ tests_lecture.cpp
@@ -16,8 +16,8 @@ int main()
     //tailles = lecture_mesh_tailles("simple_geo.mesh"); //test pour lecture des tailles -> ok
     //cout<<tailles[0]<<" "<<tailles[1]<<" "<<endl; //affichage pour test
 
-    int* tailles_back;
-    tailles_back = lecture_back_mesh_tailles("simple_geo.back.mesh"); //test pour lecture des tailles du fond -> ok
+    //int* tailles_back;
+    //tailles_back = lecture_back_mesh_tailles("simple_geo.back.mesh"); //test pour lecture des tailles du fond -> ok
     //cout<<tailles_back[0]<<" "<<tailles_back[1]<<" "<<tailles_back[2]<<endl; //affichage pour test
 
     //float ** Sol = lecture_sol("simple_geo.dist_wall.sol"); //test pour la lecture du fichier de distance au sol -> ok
@@ -28,11 +28,15 @@ int main()
     //for(int i=0;i<tailles[0];i++) {Sommets[i].print_sommet();}
     //for(int i=0;i<tailles[1];i++) {Aretes[i].print();}
 
-    sommet* Sommets = new sommet[tailles_back[0]];
-    arete* Aretes = new arete[tailles_back[1]];
-    triangle* Triangles = new triangle[tailles_back[2]];
-    lecture_back_mesh("simple_geo.back.mesh",Sommets,Aretes,Triangles); //test lecture du maillage -> ok
-    for(int i=0;i<tailles_back[2];i++) {Triangles[i].print();}
+    //sommet* Sommets = new sommet[tailles_back[0]];
+    //arete* Aretes = new arete[tailles_back[1]];
+    //triangle* Triangles = new triangle[tailles_back[2]];
+    //lecture_back_mesh("simple_geo.back.mesh",Sommets,Aretes,Triangles); //test lecture du maillage -> ok
+    //for(int i=0;i<tailles_back[2];i++) {Triangles[i].print();}
+
+    struct donnees tableaux;
+    tableaux = lecture("simple_geo.mesh","simple_geo.back.mesh","simple_geo.dist_wall.sol"); //test de la lecture globale avec renvoi d'une structure -> ok
+    cout<<tableaux.tailles_bk[2]<<endl;
 
     return 0;
 }
