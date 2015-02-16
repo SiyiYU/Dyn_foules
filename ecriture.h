0864a4aa76a5b1f3e261750b193eedadfe8d27a3
PierrePHALIPPOU <pierre.phalippou@ensta-paristech.fr>
2015-02-16 10:45:49 +0100

    Create ecriture.h

---
+++ ecriture.h
@@ -0,0 +1,13 @@
+#ifndef ECRITURE_H_INCLUDED
+#define ECRITURE_H_INCLUDED
+#include <string>
+#include <fstream>
+#include "arete.h"
+#include "triangle.h"
+#include "sommet2.h"
+#include "lecture.h"
+
+void create_text(struct donnees ,const char *);
+
+
+#endif // ECRITURE_H_INCLUDED
