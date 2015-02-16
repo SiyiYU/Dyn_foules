edc3e1626e381eda586a1ed6f5a391f574b269d9
PierrePHALIPPOU <pierre.phalippou@ensta-paristech.fr>
2015-02-16 10:39:24 +0100

    Update sommet2.h

--- sommet2.h
+++ sommet2.h
@@ -2,47 +2,48 @@
 #define MAILLAGE_DYN_H_INCLUDED
 #include <iostream>
 #include <list> //LIST des indiciduals
-
 using namespace std;
 typedef float T; // define T as float
 
+
 class sommet // means the dynamic mesh
 {
-
-    protected:
-        int ref;
-        float x,y;
-
-    public:
-        sommet(); // non init par défaut
-        void print_sommet();
-        sommet(T abs, T ord, int refer){x = abs; y = ord; ref=refer;}//Interface!!!besoin une fonction ext dans lecture à lire les possition
+protected:
+int reff;
+float xx,yy;
+public:
+sommet(); // non init par défaut
+void print_sommet();
+sommet(T abs, T ord, int refer){xx = abs; yy = ord; reff=refer;}//Interface!!!besoin une fonction ext dans lecture à lire les possition
+float x()const{return xx;};
+float y()const{return yy;};
+int ref()const{return reff;};
+void changer(float,float,int);
+sommet & operator=(const sommet & S);
 };
+
 
 class individual:public sommet
 {
-    protected:
-        int ref = 1;
-        T m,r,f_max,g;
-//        T v_x,v_y,x_x,x_y;// À calculer
-//        T vd_x,vd_y; // is the disire velosity, à calculer aussi
-
-        sommet* voisins; //the table of pointer of voisins
-
-    // on calcul rho dans les maillage.
-    public:
-        individual(T x, T y, int ref):sommet(x,y,ref){};
-        //voisin();// it calcul les voisins dans maillage dynamique
-        void allou_individual(T a,T b,T c,T d=9.8){m=a;r=b;f_max=c;g=d;cout<<"alloué"<<endl;};
-        //T calcul_fmur(T* maillage_font,T* d_mur);//on trouve lindividual est ou dans le m_font donc sais d_mur
-        //void operator ++();// returns arry of pointers of indi nearby
-        void print_individual();
-        //~individual(){delete voisins[];}
+protected:
+int reff = 1;
+T m,r,f_max,g;
+// T v_x,v_y,x_x,x_y;// À calculer
+// T vd_x,vd_y; // is the disire velosity, à calculer aussi
+sommet* voisins; //the table of pointer of voisins
+// on calcul rho dans les maillage.
+public:
+individual(T x, T y, int ref):sommet(x,y,ref){};
+//voisin();// it calcul les voisins dans maillage dynamique
+void allou_individual(T a,T b,T c,T d=9.8){m=a;r=b;f_max=c;g=d;cout<<"alloué"<<endl;};
+//T calcul_fmur(T* maillage_font,T* d_mur);//on trouve lindividual est ou dans le m_font donc sais d_mur
+//void operator ++();// returns arry of pointers of indi nearby
+void print_individual();
+//~individual(){delete voisins[];}
 };
 #endif// MAILLAGE_DYN_H_INCLUDED
 #ifndef SOMMET2_H_INCLUDED
 #define SOMMET2_H_INCLUDED
-
-
-
 #endif // SOMMET2_H_INCLUDED
+
+
