2d5aaed3552f43f8aa792e95643721c8b6882c22
PierrePHALIPPOU <pierre.phalippou@ensta-paristech.fr>
2015-02-20 14:17:31 +0100

    Update sommet2.cpp

--- sommet2.cpp
+++ sommet2.cpp
@@ -5,26 +5,60 @@
 using namespace std;
 typedef float T;
 sommet::sommet(){xx=0;yy=0;reff=0;}
-void sommet::print_sommet()
+
+/*void sommet::print_sommet()
 {
 cout<<"Position: "<<xx<<"\t"<<yy<<endl;
 cout<<"Reference: "<<reff<<endl;
 }
+
 void individual::print_individual()
 {
 cout<<"m,r,f_max,g:"<<endl<<m<<"\t"<<r<<"\t"<<f_max<<"\t"<<g<<endl;
 cout<<"ref = "<<reff<<endl;
 }
+
 sommet & sommet::operator=(const sommet & S){
   xx=S.x();
   yy=S.y();
   reff=S.ref();
   return *this;
-}
+}*/
 
-void sommet::changer(float x,float y,int r){
+void sommet::changer(float x,float y, float vx, float vy, int r){
   xx=x;
   yy=y;
+  reff=r;
+}
+
+void sommet::masse(float ma){
+}
+
+void sommet::g(float gw){
+}
+
+void sommet::objectif(float x, float y){
+}
+
+void individual::masse(float ma){
+  m=ma;
+}
+
+void individual::g(float gw){
+  gg=gw;
+}
+
+void individual::objectif(float x, float y){
+  obj_x=x;
+  obj_y=y;
+}
+
+
+void individual::changer(float x, float y, float vx, float vy, int reff){
+  xx=x;
+  yy=y;
+  v_x=vx;
+  v_y=vy;
   reff=r;
 }
 
