436950b47766a126c1248fca02146a4f9a541fd0
PierrePHALIPPOU <pierre.phalippou@ensta-paristech.fr>
2015-02-20 14:12:43 +0100

    Update ajouter_individu.h

--- ajouter_individu.h
+++ ajouter_individu.h
@@ -1,6 +1,7 @@
 #ifndef AJOUTER_INDIVIDU_H_INCLUDED
 #define AJOUTER_INDIVIDU_H_INCLUDED
 
-void ajouter_individu(float ,float ,struct donnees &);
+void ajouter_individu(float ,float ,struct donnees &, float ,float, float ,float);
+//( x , y , Donnees , objectif x , objectif y , gw )
 
-#endif // AJOUTER_INDIVIDU_H_INCLUDED
+#endif 
