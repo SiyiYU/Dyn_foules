d04c0cca1e29af061ca345c1f01d2bd3a031a73c
BarbaraPerri <perri@ensta.fr>
2015-03-06 10:00:19 +0100

    Create maillage_fr.h

---
+++ maillage_fr.h
@@ -0,0 +1,9 @@
+#ifndef MAILLAGE_FR_H_INCLUDED
+#define MAILLAGE_FR_H_INCLUDED
+#include "donnees.h"
+#include "sommet2.h"
+
+void insert(donnees, sommet, const char*);
+void swap(donnees, const char*);
+
+#endif // MAILLAGE_FR_H_INCLUDED
