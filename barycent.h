b82130a2167a742ae32ecba1029a933a4aada98f
BarbaraPerri <perri@ensta.fr>
2015-03-06 09:58:33 +0100

    Create barycent.h

---
+++ barycent.h
@@ -0,0 +1,8 @@
+#ifndef BARYCENT_H_INCLUDED
+#define BARYCENT_H_INCLUDED
+#include "triangle.h"
+#include "donnees.h"
+
+float* barycent(float,float,triangle, donnees);
+
+#endif // BARYCENT_H_INCLUDED
