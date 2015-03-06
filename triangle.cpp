6f24796e96582d3d54aa01b6a25026c0e7f33631
BarbaraPerri <perri@ensta.fr>
2015-03-06 09:54:55 +0100

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -8,7 +8,8 @@ triangle::triangle()
     nb1 = 0;
     nb2 = 0;
     nb3 = 0;
-    reference = 0;
+    reference = 1;
+    vivant = 1;
 }
 
 triangle::triangle(int sommet1, int sommet2, int sommet3, int refer)
@@ -17,9 +18,55 @@ triangle::triangle(int sommet1, int sommet2, int sommet3, int refer)
     nb2 = sommet2;
     nb3 = sommet3;
     reference = refer;
+    vivant = 1;
 }
 
 void triangle::print()
 {
     cout<<nb1<<" "<<nb2<<" "<<nb3<<" "<<reference<<endl;
+}
+
+triangle & triangle::operator = (triangle t)
+{
+    nb1 = t.sommet1();
+    nb2 = t.sommet2();
+    nb3 = t.sommet3();
+    reference = t.ref();
+    return(*this);
+}
+
+bool triangle::have_edge(arete a)
+{
+    if(nb1 == a.sommet1())
+    {
+        if(nb2 == a.sommet2()) {return(true);}
+        else
+        {
+            if(nb3 == a.sommet2()) {return(true);}
+            else {return(false);}
+        }
+    }
+    else if(nb1 == a.sommet2())
+    {
+        if(nb2 == a.sommet1()) {return(true);}
+        else
+        {
+            if(nb3 == a.sommet1()) {return(true);}
+            else {return(false);}
+        }
+    }
+    else
+    {
+        if(nb2 == a.sommet1())
+        {
+            if(nb3 == a.sommet2()) {return(true);}
+            else {return(false);}
+        }
+        else if(nb2 == a.sommet2())
+        {
+            if(nb3 == a.sommet1()) {return(true);}
+            else {return(false);}
+        }
+        else {return(false);}
+    }
 }
