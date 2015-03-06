0ddc7c818b1a64145c487f3c79b8be48b5c2a7ae
PierrePHALIPPOU <pierre.phalippou@ensta-paristech.fr>
2015-03-06 10:35:15 +0100

    Update sommet2.cpp

--- sommet2.cpp
+++ sommet2.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdlib>
 #include "sommet2.h"
+
 using namespace std;
 
 typedef float T;
@@ -10,14 +11,16 @@ sommet::sommet(){xx=0;yy=0;reff=0;}
 
 void sommet::print_sommet()
 {
-cout<<"Position: "<<xx<<"\t"<<yy<<endl;
-cout<<"Reference: "<<reff<<endl;
+  cout<<"Position: "<<xx<<"\t"<<yy<<endl;
+  cout<<"Reference: "<<reff<<endl;
 }
+
 void individual::print_individual()
 {
-cout<<"m,r,f_max,g:"<<endl<<m<<"\t"<<r<<"\t"<<f_max<<"\t"<<g<<endl;
-cout<<"ref = "<<reff<<endl;
+  //cout<<"m,r,f_max,g:"<<endl<<m<<"\t"<<r<<"\t"<<f_max<<"\t"<<g<<endl;
+  //cout<<"ref = "<<reff<<endl;
 }
+
 sommet & sommet::operator=(const sommet & S){
   xx=S.x();
   yy=S.y();
@@ -28,6 +31,71 @@ sommet & sommet::operator=(const sommet & S){
 void sommet::changer(float x,float y,int r){
   xx=x;
   yy=y;
+  reff=r;
+}
+
+void sommet::masse(float ma){
+}
+
+void sommet::g(float gw){
+}
+
+void sommet::objectif(float x, float y){
+}
+
+sommet::sommet(const sommet & s){
+  reff=s.ref();
+  xx=s.x();
+  yy=s.y();
+}
+
+individual:individual(){
+  reff=0;
+  m=0,;
+  rr=0;
+  f_max=0;
+  gg=0;
+  v_x=0;
+  v_y=0;
+  xx=0;
+  xy=0;
+  obj_x=0;
+  obj_y=0;
+}
+
+void individual::masse(float ma){
+  m=ma;
+}
+
+void individual::g(float gw){
+  gg=gw;
+}
+
+individual:individual(const individual & i){
+  reff=i.ref();
+  m=i.masse();
+  rr=i.r();
+  f_max=i.fmax();
+  xx=i.x();
+  yy=i.y();
+  v_x=i.vx();
+  v_y=i.vy();
+  gg=i.gw();
+  obj_x=i.objx();
+  obj_y=i.objy();
+  voisins=i.vois();
+}
+
+void individual::objectif(float x, float y){
+  obj_x=x;
+  obj_y=y;
+}
+
+void individual::change(float x, float y, float vx, float vy, int reff){
+  xx=x;
+  yy=y;
+  v_x=vx;
+  v_y=vy;
   reff=r;
 }
 
