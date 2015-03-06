942570db6f95556c3ff4b4918c23f4acfd09b491
BarbaraPerri <perri@ensta.fr>
2015-03-06 09:59:22 +0100

    Create ecriture.h

---
+++ ecriture.h
@@ -0,0 +1,12 @@
+#ifndef ECRITURE_H_INCLUDED
+#define ECRITURE_H_INCLUDED
+#include <string>
+#include <fstream>
+#include "arete.h"
+#include "triangle.h"
+#include "sommet2.h"
+#include "lecture.h"
+
+void create_text(donnees ,const char *);
+
+#endif // ECRITURE_H_INCLUDED
