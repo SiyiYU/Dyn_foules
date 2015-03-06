809aba568c8e2d97c525265ab69e72d2ec38abf4
BarbaraPerri <perri@ensta.fr>
2015-03-06 09:54:14 +0100

    Update lecture.h

--- lecture.h
+++ lecture.h
@@ -6,28 +6,14 @@
 #include "arete.h"
 #include "triangle.h"
 #include "sommet2.h"
+#include "donnees.h"
 
 using namespace std;
-
-struct donnees
-{
-    int* tailles_fr;
-    sommet* Sommets_fr;
-    arete* Aretes_fr;
-    triangle* Triangles_fr;
-
-    int* tailles_bk;
-    sommet* Sommets_bk;
-    arete* Aretes_bk;
-    triangle* Triangles_bk;
-
-    float** Sol;
-};
 
 int* lecture_mesh_tailles(const char* file); //renvoie un tableau avec le nombre de sommets, le nombre d'arÃªtes
 void lecture_mesh(const char* file, sommet* Vertices, arete* Edges, triangle* Triangles); //permet d'avoir le tableau des sommets et celui des aretes pour le maillage de devant
 float** lecture_sol(const char* file); //renvoie le tableau avec les distances au sol
 
-struct donnees lecture(const char* mesh, const char* back_mesh, const char* sol);
+//donnees lecture(const char* mesh, const char* back_mesh, const char* sol);
 
 #endif // LECTURE_H_INCLUDED
